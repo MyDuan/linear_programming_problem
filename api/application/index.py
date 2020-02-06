902e2b1b84b8b8868eb02c6307f7f8a89997511b
MyDuan <dy1226130575@163.com>
2020-02-06 17:29:43 +0900

    Small fixed.

--- api/application/index.py
+++ api/application/index.py
@@ -1,5 +1,14 @@
 from flask import render_template, session
 
 
-def show():
-    return render_template('index.html')
+def show(data):
+    if session.get('results'):
+        if data:
+            return render_template('index.html', results=session['results'], data=data)
+        else:
+            return render_template('index.html', results=session['results'])
+    else:
+        if data:
+            return render_template('index.html', data=data)
+        else:
+            return render_template('index.html')
